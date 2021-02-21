09c2163bb93a12991757a1e39a8ca0fc59a01a26
Yunus Emre Eke <47570969+yemreeke@users.noreply.github.com>
2021-02-21 12:08:02 +0300

    Update HW3.py

--- Homeworks/HW3.py
+++ Homeworks/HW3.py
@@ -1,1 +1,22 @@
-Boşşşşşş
+def prime_first(number):
+    for i in range(2,int((number**0.5)+1)):
+        if(number%i==0):
+            return False
+    if(number==1 or number==0):
+        return False
+    return True
+def prime_second(number):
+    for i in range(2,int((number**0.5)+1)):
+        if(number%i==0):
+            return False
+    if(number==1 or number==0):
+        return False
+    return True
+
+for i in range(0,1000+1):
+    if(i<500):
+        if(prime_first(i)):
+            print(i)
+    else:
+        if(prime_second(i)):
+            print(i)
